1e6c5039f0475d3691356ad0bb8ecd1779485442
nobel7761 <habiburnobel@gmail.com>
2023-10-29 21:26:32 +0600

    NotFoundException implemented in get message by id

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Get, Post, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
 import { MessagesService } from './messages.service';
 
@@ -18,10 +25,16 @@ export class MessagesController {
   }
 
   @Get('/:id')
-  getSingleMessage(@Param('id') id: string) {
+  async getSingleMessage(@Param('id') id: string) {
     //we need to extract "/:id" out of the root of the route from the incoming request
     console.log(id);
-    return this.messagesService.findOne(id);
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('Message not found!');
+    }
+
+    return message;
   }
 
   @Post()
