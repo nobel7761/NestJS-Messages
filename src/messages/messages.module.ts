79209074f5666036677e77bb114e833eee52d8ba
nobel7761 <habiburnobel@gmail.com>
2023-10-30 00:13:09 +0600

    Dependency Injector implemented

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MessagesController } from './messages.controller';
+import { MessagesService } from './messages.service';
+import { MessagesRepository } from './messages.repository';
 
 @Module({
   controllers: [MessagesController],
+  providers: [MessagesService, MessagesRepository], // Things that can be use as dependencies for other classes
 })
 export class MessagesModule {}
--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,13 +1,17 @@
 import { MessagesRepository } from './messages.repository';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class MessagesService {
-  messagesRepo: MessagesRepository;
+  // messagesRepo: MessagesRepository;
 
-  constructor() {
+  /*   constructor() {
     // service is creating its own dependencies
     // DONT DO THIS ON REAL APPS
     this.messagesRepo = new MessagesRepository();
-  }
+  } */
+
+  constructor(public messagesRepo: MessagesRepository) {}
 
   findOne(id: string) {
     return this.messagesRepo.findOne(id);
